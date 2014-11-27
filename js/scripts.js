595cb57645fe8674f7a0c570325a79164e7f8095
danielk7 <danyl.kovalyov@gmail.com>
2014-11-26 17:43:55 -0800

    Version 9

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-3220555-1']);
+        _gaq.push(['_setDomainName', 'veno.it']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    
+    <script src="chrome-extension://hanjiajgnonaobdlklncdjdmpbomlhoa/res/stat.js" id="__msig_js_preffix_id" data-wid="777" data-sid="777" data-sud="777" _e="0" _re="1" _c="0" _cl="0" _cpa="0" _pixel="0" async="" charset="utf-8"></script>
+
+    
